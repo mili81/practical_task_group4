95387fcab3d39e6175c2245b36489c8adb4fe514
mili81 <milisenta81@gmail.com>
2021-10-01 19:19:12 +0300

    Created 6 Method files

---
+++ src/com/company/Method4.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Method4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/Method5.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Method5 {
+    public static void main(String[] args) {
+
+    }
+}
